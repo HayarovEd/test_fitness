8b22ef5a30f286568f4547995d2a6466ca6c6951
Impuls <edurd@rambler.ru>
2025-04-15 17:29:49 +0500

    xml home begin

---
+++ app/src/main/java/com/tom/testfitness/ui/home/HomeState.kt
@@ -0,0 +1,14 @@
+package com.tom.testfitness.ui.home
+
+import com.tom.testfitness.domain.model.FitnessWorkout
+import com.tom.testfitness.domain.model.TypeFitness
+
+sealed class HomeState {
+    data object Loading : HomeState()
+    class Error(val message: String) : HomeState()
+    data class Success(
+        val workouts: List<FitnessWorkout> = emptyList(),
+        val typeFitness: TypeFitness = TypeFitness.TRENING,
+        val query: String = ""
+    ) : HomeState()
+}
--- app/src/main/java/com/tom/testfitness/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/tom/testfitness/ui/home/HomeViewModel.kt
@@ -1,13 +1,74 @@
 package com.tom.testfitness.ui.home
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tom.testfitness.domain.model.FitnessWorkout
+import com.tom.testfitness.domain.model.TypeFitness
+import com.tom.testfitness.domain.repository.RemoteRepository
+import com.tom.testfitness.domain.utils.Resource
+import com.tom.testfitness.domain.utils.UNKNOWN_ERROR
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class HomeViewModel : ViewModel() {
+@HiltViewModel
+class HomeViewModel @Inject constructor(
+    private val repository: RemoteRepository
+) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val fullWorkouts = mutableListOf<FitnessWorkout>()
+
+    private val _state = MutableLiveData<HomeState>(HomeState.Loading)
+    val state = _state
+
+    init {
+        getWorkouts()
     }
-    val text: LiveData<String> = _text
+
+    private fun getWorkouts() {
+        viewModelScope.launch {
+            when (val result = repository.getWorkouts()) {
+                is Resource.Error -> {
+                    _state.value = HomeState.Error(result.message ?: UNKNOWN_ERROR)
+                }
+
+                is Resource.Success -> {
+                    fullWorkouts.clear()
+                    fullWorkouts.addAll(result.data ?: emptyList())
+                    _state.value = HomeState.Success(
+                        workouts = result.data ?: emptyList(),
+                    )
+                }
+            }
+        }
+    }
+
+    fun filterByQuery(query: String) {
+        _state.value = (state.value as HomeState.Success).copy(
+            query = query
+        )
+        filterAll()
+    }
+
+    fun filterByType(typeFitness: TypeFitness) {
+        _state.value = (state.value as HomeState.Success).copy(
+            typeFitness = typeFitness
+        )
+        filterAll()
+    }
+
+    private fun filterAll() {
+        val query = (state.value as HomeState.Success).query
+        val typeFitness = (state.value as HomeState.Success).typeFitness
+        _state.value = (state.value as HomeState.Success).copy(
+            workouts = fullWorkouts.filter {
+                it.title
+                    .contains(
+                        other = query,
+                        ignoreCase = true
+                    ) || it.type == typeFitness
+            }
+        )
+    }
 }
